b086c6af7a069871239d3d1e32e2d3f1c43898eb
ViniciusBPessoa <viniciusbpessoa02@gmail.com>
2023-02-16 23:01:31 -0300

    Resposta de todas as questões tradicionais
    Respostas enviadas aqui: 1, 2, 3, 4.

---
+++ Questoes tardicionais/q4_a.asm
@@ -0,0 +1,48 @@
+.data
+  
+  mensagem1: .asciiz "mansagem test" # Carrega a messagem a ser copiada
+  espaco: .space 200 # Carrega o espaço na memora para o qual a copia irá
+  
+.text
+  
+main: 
+  
+  la $a0, espaco # separa o espaço na memoria em a0
+  la $a1, mensagem1 #carrega em a1 a mensagema ser copiada
+  
+  jal strcpy # pula para a função
+  
+  addi $a0, $v0, 0 # adiciona o retorno a0 o retorno para testar a resposta
+  jal imprime_string  # imprime o teste
+  
+  j fim # pula para o fim do programa
+  
+strcpy:
+
+  addi $t2, $a0, 0  # adiciona os endereços a t2
+  addi $t3, $a1, 0  # adiciona os endereços as t3
+  
+  loop:
+  
+    lb $t1, 0($t3) # carrega em t1 o conteudo de a0 no momento
+    addi $t3, $t3, 1  # pula para a proxima casa de a0
+    
+    sb $t1, 0($t2) # carrega em t2 o conteudo de a1 no momento
+    addi $t2, $t2, 1  # pula para a proxima casa de a0
+  
+    bne $t0, $t1, loop # cetificace de que a string ainda não acabou
+  
+  lb $t1, 0($t3) # adiciona o /0 ao fim da string
+  sb $t1, 0($t2)  # adiciona o /0 ao fim da string
+  addi $v0, $a0, 0  # retorna a função em v0
+  jr $ra  # rotorna ao fluxo normal
+
+
+imprime_string:						#Recebe a string a ser lida em $a0
+    addi $v0, $0, 4						#Chama a função imprimir string
+    syscall						#Executa a função
+    jr $ra						#Retorna pra quem chamou o procedimento
+    
+fim: # finaliza o codigo
+  addi $v0, $0,  10   # Armazena o código da syscall para finalizar o codigo
+  syscall
--- Questão1.asm
+++ Questão1.asm
@@ -1,43 +1,68 @@
-# Receber uma string do usuário em MIPS Assembly
+# Substituir caracteres em uma string em MIPS Assembly
 
 .data
-	frase: .word 201 # numero maximo de bites para caracteres que o usuario podera digitar
-	c1: .word 3
-	c2: .word 3
+mensagem1: .asciiz "Digite uma string: "
+string: .space 100
+mensagem2: .asciiz "Digite o caractere a ser substituído (C1): "
+caractere1: .byte 0
+mensagem3: .asciiz "Digite o caractere substituto (C2): "
+caractere2: .byte 0
+
 .text
-
+.globl main
 main:
+    # Imprimir a mensagem solicitando a string
+    li $v0, 4
+    la $a0, mensagem1
+    syscall
 
-    addi $v0, $0, 8 # soma(8 + 0) para iniciar o protocolo de impreção de valores no console
-    la $a0, frase
-    addi $a1, $0, 201
-    la $s0, frase
-    syscall # o sistema executa o que foi solicitado
-    
-    addi $v0, $0, 8 # soma(8 + 0) para iniciar o protocolo de impreção de valores no console
-    la $a0, c1
-    addi $a1, $0, 3
-    la $s1, c1
-    syscall # o sistema executa o que foi solicitado
-    
-    addi $v0, $0, 8 # soma(8 + 0) para iniciar o protocolo de impreção de valores no console
-    la $a0, c2
-    addi $a1, $0, 3
-    la $s2, c2
-    syscall # o sistema executa o que foi solicitado
-    
-    addi $v0, $0, 4 # soma(4 + 0) para iniciar o protocolo de impreção de valores no console
-    la $a0, frase # adiciona em a0 o valor a ser impresso na tela
-    syscall # o sistema executa o que foi solicitado
-    
-    addi $v0, $0, 4 # soma(4 + 0) para iniciar o protocolo de impreção de valores no console
-    la $a0, c1 # adiciona em a0 o valor a ser impresso na tela
-    syscall # o sistema executa o que foi solicitado
-    
-    addi $v0, $0, 4 # soma(4 + 0) para iniciar o protocolo de impreção de valores no console
-    la $a0, c2 # adiciona em a0 o valor a ser impresso na tela
-    syscall # o sistema executa o que foi solicitado
-    
+    # Receber a string do usuário
+    li $v0, 8
+    la $a0, string
+    li $a1, 100
+    syscall
+
+    # Imprimir a mensagem solicitando o caractere 1
+    li $v0, 4
+    la $a0, mensagem2
+    syscall
+
+    # Receber o caractere 1 do usuário
+    li $v0, 12
+    syscall
+    sb $v0, caractere1
+
+    # Imprimir a mensagem solicitando o caractere 2
+    li $v0, 4
+    la $a0, mensagem3
+    syscall
+
+    # Receber o caractere 2 do usuário
+    li $v0, 12
+    syscall
+    sb $v0, caractere2
+
+    # Substituir caractere 1 por caractere 2 na string
+    la $s0, string        # Endereço da string
+    lb $s1, caractere1    # Caractere 1
+    lb $s2, caractere2    # Caractere 2
+
+loop:
+    lb $t0, 0($s0)        # Carregar o próximo caractere da string
+    beq $t0, $0, fim      # Se o caractere for nulo, encerrar o loop
+    bne $t0, $s1, next    # Se o caractere não for igual ao caractere 1, ir para o próximo
+    sb $s2, 0($s0)        # Substituir o caractere 1 pelo caractere 2
+
+next:
+    addi $s0, $s0, 1      # Avançar para o próximo caractere
+    j loop
+
+fim:
+    # Imprimir a nova string
+    li $v0, 4
+    la $a0, string
+    syscall
+
     # Encerrar o programa
-    addi $v0, $0, 10
+    li $v0, 10
     syscall
